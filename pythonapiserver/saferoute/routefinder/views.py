c7d49c145f860ee859d6162830c65787d142b878
iverma1 <ishita_verma@live.com>
2014-04-12 15:12:47 -0700

    completed response format for directions

--- pythonapiserver/saferoute/routefinder/views.py
+++ pythonapiserver/saferoute/routefinder/views.py
@@ -31,6 +31,12 @@ def geocode(request):
 
 DIRECTION_BASE_URL = 'http://maps.googleapis.com/maps/api/directions/json?sensor=false&alternatives=true'
 
+def directions(request):
+    #TODO: make use of origin and destination coordiantes in request
+    result = get_directions_from_google()
+    result = modify_result(result.read())
+    return HttpResponse(json.dumps(result), content_type="application/json")
+
 
 def get_directions_from_google(origin="37.3909762,-122.0663274", destination="37.4909762,-122.0663274", **geo_args):
     geo_args.update({
@@ -43,33 +49,46 @@ def get_directions_from_google(origin="37.3909762,-122.0663274", destination="37
     #TODO: parse xml to return warnings for each step in every left
     return urllib.urlopen(url)
 
-def get_warning_for_step(start, end):
-    #TODO: read for cumulative warning along the path from start to end
-    return 200
-
-
-def directions(request):
-    #TODO: make use of origin and destination coordiantes in request
-    result = get_directions_from_google()
-    result = modify_result(result.read())
-    return HttpResponse(json.dumps(result), content_type="application/json")
-
 
 def modify_result(html):
-    #TOD0: iterate instead of 0
     complete_response = json.loads(html)
     routes = complete_response["routes"]
-    legs = routes[0]["legs"]
-    steps = legs[0]["steps"]
-    step = steps[0]
+    #create route_risk wrappers
+    route_risks = []
 
-    risk = find_risk(step["start_location"]["lat"], step["start_location"]["lng"],
-                     step["end_location"]["lat"], step["end_location"]["lng"])
+    for route in routes:
+        total_route_risk = 0
+        route_risk = {}
 
-    #TODO: add back risk to html
+        #extract steps in a route
+        legs = route["legs"]
+        steps = legs[0]["steps"]
+
+        #for each step append to step_risks and add to total risk of route
+        step_risks = []
+        for step in steps:
+            avg_loc = average_out_path(step["start_location"]["lat"], step["start_location"]["lng"],
+                        step["end_location"]["lat"], step["end_location"]["lng"])
+            step_risk = find_risk(avg_loc)
+            step_risks.append({"lat": avg_loc[0], "lng": avg_loc[1], "risk": step_risk})
+            total_route_risk += step_risk
+
+        #populate route risk with total risk of its steps and risk breakdown
+        route_risk["total_risk"] = total_route_risk
+        route_risk["risk_breakdown"] = step_risks
+
+        #for each route append to route risks at root level
+        route_risks.append(route_risk)
+
+    complete_response["risks"] = route_risks
+
     return complete_response
 
 
-def find_risk(start_lat, start_lng, end_lat, eng_lng):
+def average_out_path(start_lat, start_lng, end_lat, end_lng):
+    return [(start_lat + end_lat)/2, (start_lng + end_lng)/2]
+
+
+def find_risk(loc):
     #TOD0: add database call
-    return start_lat+start_lng+end_lat+eng_lng
+    return 200
