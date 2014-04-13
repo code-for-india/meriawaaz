0d9f25a3f14ab358cde84a809973e83c5fa4d722
rohit <nrohits@gmail.com>
2014-04-13 13:23:09 -0700

    adding rest api for incidents:

---
+++ pythonapiserver/saferoute/incidentapi/views.py
@@ -0,0 +1,53 @@
+"""
+APIs for getting and inserting incident related data.
+"""
+import urllib
+import json
+from django.http import HttpResponse, HttpResponseBadRequest
+from django.views.decorators.csrf import csrf_exempt
+from models import Incident, IncidentManager
+from django.core import serializers
+
+@csrf_exempt
+def incidents_handler(request):
+  if request.method == 'GET':
+    incidents = Incident.objects.all()
+    mod_incidents = []
+    for item in incidents:
+      mod_incidents.append(item.to_dict())
+    return HttpResponse(json.dumps(mod_incidents), content_type="application/json")
+  
+  elif request.method == 'POST':
+    if request.META['CONTENT_TYPE'] == 'application/json':
+      data = json.loads(request.body)
+      i_manager = IncidentManager()
+      incident = i_manager.load_from_dict(data)
+      incident.save()
+      return HttpResponse(json.dumps(incident.to_dict()), content_type="application/json")
+    else:
+      return HttpResponseBadRequest('Request content is not application/json.')
+  
+  else:
+    return HttpResponseBadRequest('This method is not supported.')
+
+
+@csrf_exempt
+def incident_handler(request, incident_id):
+  try:
+    incident = Incident.objects.get(uid=incident_id)
+    if request.method == 'GET':
+      json_data = json.dumps(incident.to_dict())
+      return HttpResponse(json_data, content_type="application/json")
+    
+    elif request.method == 'PUT' or request.method == 'POST':
+      data = json.loads(request.body)
+      i_manager = IncidentManager()
+      incident = i_manager.load_from_dict(data, incident)
+      incident.save()
+      return HttpResponse(json.dumps(incident.to_dict()), content_type="application/json")
+    
+    else:
+      return HttpResponseBadRequest()
+  
+  except Incident.DoesNotExist:
+    return HttpResponse("", content_type="application/json")
