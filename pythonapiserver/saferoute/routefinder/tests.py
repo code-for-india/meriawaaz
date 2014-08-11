93e3c10d230c612d91f3fd85d50e709367c7e01f
iverma1 <ishita_verma@live.com>
2014-08-10 20:19:00 -0700

    with nevan: tested and created a marginal wrapper polygon around path to look for risks around

--- pythonapiserver/saferoute/routefinder/tests.py
+++ pythonapiserver/saferoute/routefinder/tests.py
@@ -5,12 +5,45 @@ when you run "manage.py test".
 Replace this with more appropriate tests for your application.
 """
 
-from django.test import TestCase
+from routefinder.views import create_polygon_wrapper
+
+#from django.test import TestCase
+import unittest
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class RouteFinderTests(unittest.TestCase):
+    def test_polygon_wrapper_along_route(self):
+        legs_of_unit_diagnol = (0.5)**.5
+        expected_results = [[10, -1], [-legs_of_unit_diagnol, -legs_of_unit_diagnol], [-1, 10],
+                            [1, 10], [legs_of_unit_diagnol, legs_of_unit_diagnol], [10, 1]]
+        inpoints = [[0, 10], [0, 0], [10, 0]]
+
+        outpoints = create_polygon_wrapper(inpoints, 1)
+       # print(outpoints)
+       # print(expected_results)
+
+        self.assertEqual(len(outpoints), len(expected_results))
+
+        for i in range(len(inpoints)):
+            for cord in range(2):
+                self.assertAlmostEqual(outpoints[i][cord], expected_results[i][cord], delta=.01)
+
+    def test_polygon_wrapper_along_route2(self):
+        lud = (0.5)**.5
+        expected_results = [[0, -11], [10+lud, -10-lud], [10+lud, lud], [lud, lud], [lud, 20-lud], [10-lud, 20-lud],
+                            [9, 10], [11, 10], [10+lud, 20+lud], [-lud, 20+lud], [-lud, -lud], [10-lud, -lud], [10-lud,
+                            -10+lud], [0, -9]][::-1]
+        inpoints= [[10, 10], [10, 20], [0, 20], [0, 0], [10, 0], [10, -10], [0, -10]]
+
+        outpoints = create_polygon_wrapper(inpoints, 1)
+        print(outpoints)
+        print()
+        print(expected_results)
+
+        self.assertEqual(len(outpoints), len(expected_results))
+
+        for i in range(len(inpoints)):
+            for cord in range(2):
+                self.assertAlmostEqual(outpoints[i][cord], expected_results[i][cord], delta=.01)
+
+unittest.main()
--- pythonapiserver/saferoute/routefinder/views.py
+++ pythonapiserver/saferoute/routefinder/views.py
@@ -87,36 +87,64 @@ def add_risk_element_to_result(html):
 
 PADDING = 100
 
-
-def create_polygon_wrapper(points):
+# Creates a polygon around the line
+# This is used to find the polygon to query the database with
+def create_polygon_wrapper(inpoints, padding):
     results = []
-    for i in range(1, len(points)-1):
-        previous = points[i-1]
-        current = points[i]
-        after = points[i+1]
+    for i in range(len(inpoints)):
+        current = inpoints[i]
+        if i < len(inpoints) - 1:
+            after = inpoints[i+1]
+            next_theta = math.atan2(after[1]-current[1], after[0]-current[0])
 
+        previous = inpoints[i-1]
         prev_theta = math.atan2(previous[1]-current[1], previous[0]-current[0])
-        next_theta = math.atan2(after[1]-current[1], after[0]-current[0])
 
-        bisector_theta = (prev_theta + next_theta)/2
+        #if we're at the first or last point, pretend that the route extends straight beyond that
+        #this will let us draw the box correctly
+        if i == 0:
+            prev_theta = next_theta + math.pi
+        if i == len(inpoints) - 1:
+            next_theta = prev_theta + math.pi
 
-        new_p = [[PADDING * math.cos(bisector_theta), PADDING * math.sin(bisector_theta)],
-                 [PADDING * math.cos(bisector_theta + math.pi), PADDING * math.sin(bisector_theta + math.pi)]]
+        bisector_theta = ((prev_theta + next_theta)/2) % math.pi
+        print(inpoints[i])
+        print(bisector_theta)
+        print(bisector_theta + math.pi)
+
+        inside_range = [prev_theta % (2 * math.pi)]
+        inside_range.append(inside_range[0] + math.pi)
+
+        new_p = [[padding * math.cos(bisector_theta), padding * math.sin(bisector_theta)],
+                 [padding * math.cos(bisector_theta + math.pi), padding * math.sin(bisector_theta + math.pi)]]
+
+
+        if (bisector_theta > inside_range[0] and bisector_theta < inside_range[1]) or (bisector_theta > inside_range[0] - math.pi * 2 and bisector_theta < inside_range[1] - math.pi * 2):
+                tmp = new_p[1]
+                new_p[1] = new_p[0]
+                new_p[0] = tmp
 
         for p in range(len(new_p)):
             new_p[p][0] += current[0]
             new_p[p][1] += current[1]
 
+
+
         results.append(new_p[0])
         results.insert(0, new_p[1])
 
-        return results
+    return results
 
 
 def average_out_path(start_lat, start_lng, end_lat, end_lng):
     #for now we assume paths are mostly straight and use it's linear mean
     return [(start_lat + end_lat)/2, (start_lng + end_lng)/2]
 
+def wolf_print(points):
+    out = []
+    for point in points:
+        out.append(tuple(point))
+    return tuple(out)
 
-points = [(-4, 4), (-2, 0), (1, -4)]
-print(create_polygon_wrapper(points))
+#points = [[-4, 4], [-2, 0], [1, -4]]
+#print(wolf_print(create_polygon_wrapper(points, 1)), wolf_print(points))
