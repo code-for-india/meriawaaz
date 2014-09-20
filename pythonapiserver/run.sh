90fb62146b7152716f83d3826868e56205d9398c
ashrith <svishal.raj@gmail.com>
2014-09-20 16:26:31 -0700

    Run the script to launch the server

---
+++ pythonapiserver/run.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+if  type "virtualenv" > /dev/null 2>&1; then
+  echo "=============Setting environment============"
+  virtualenv myenvironment
+  source myenvironment/bin/activate
+  pip install -r requirements.txt
+  cd saferoute
+  echo "Open the browser with the URL http://localhost:8000"
+  python manage.py runserver
+  else 
+     sudo yum install virtualenv
+fi
